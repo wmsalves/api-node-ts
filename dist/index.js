3d272e2c684633f54f5547cc71e8bdbd6abc8716
Wemerson <wemersoncontas22@gmail.com>
2024-07-18 17:50:36 -0300

    build: re-build project

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const knex_1 = require("./database/knex");
+const knex_1 = require("./database/knex/environment.js");
 const server_1 = require("./server");
 const startServer = () => {
     server_1.server.listen(process.env.PORT || 3000, () => {
