22caacc36d3eaea4def4c967edb9e071f33b9270
Wemerson <wemersoncontas22@gmail.com>
2024-07-22 14:44:47 -0300

    tests: updated tests to persons and cities ( 1/2 )

--- tests/cities/create.test.ts
+++ tests/cities/create.test.ts
@@ -2,10 +2,35 @@ import { StatusCodes } from "http-status-codes";
 import { testServer } from "../jest.setup";
 
 describe("Cities - Create", () => {
+  let accessToken = "";
+  beforeAll(async () => {
+    const email = "create-cities@gmail.com";
+    await testServer
+      .post("/register")
+      .send({ name: "Teste", email, password: "123456" });
+    const signInRes = await testServer
+      .post("/login")
+      .send({ email, senha: "123456" });
+
+    accessToken = signInRes.body.accessToken;
+  });
+
+  it("Tenta criar registro sem token de acesso"), async () => {
+
+    const res1 = await testServer.post("/cities").send({name: "Belo Horizonte"});
+
+    expect(res1.statusCode).toEqual(StatusCodes.UNAUTHORIZED);
+    expect(typeof res1.body).toHaveProperty('errors.default');
+  }
+
+
   it("Criar registro", async () => {
-    const res1 = await testServer.post("/cities").send({
-      name: "Belo Horizonte",
-    });
+    const res1 = await testServer
+      .post("/cities")
+      .set({ Authorization: `Bearer ${accessToken}` })
+      .send({
+        name: "Belo Horizonte",
+      });
 
     expect(res1.statusCode).toEqual(StatusCodes.CREATED);
     expect(typeof res1.body).toEqual("number");
